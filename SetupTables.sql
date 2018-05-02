037a5476392d181cb2a750a93309420224b47e40
Tenma Rollins <tenmarollins@gmail.com>
2018-05-02 13:57:47 -0700

    modified backend to ensure single-verification attempts (no multiple pingings of the server to get multiple success messages for verification allowed, only the first correct verification should return success). Also changed login to allow for a field called verify to be passed with the json object to tell the caller whether or not the user is verified.
    later it will be necessary to make sure that any member that attempts to send a message or add a contact is verified, just to be completely sure there isnt some weird person-in-the-middle attack going on.

--- SetupTables.sql
+++ SetupTables.sql
@@ -6,7 +6,8 @@ CREATE TABLE Members (MemberID SERIAL PRIMARY KEY,
                       Email VARCHAR(48) NOT NULL UNIQUE,
                       Password VARCHAR(255) NOT NULL,
                       SALT VARCHAR(255),
-                      Verification INT DEFAULT 0
+                      Verification INT DEFAULT 0,
+                      VerificationCode INT
 );
 
 DROP TABLE IF EXISTS Contacts;
--- utilities/sql_conn.js
+++ utilities/sql_conn.js
@@ -3,7 +3,7 @@ const pgp = require('pg-promise')();
 pgp.pg.defaults.ssl = true;
 //Create connection to Heroku Database
 const db =
-    pgp('postgres://rknopqdrtyqmys:23f13f349047d4cdf167680278e147263a2cfc0de10b5871f0aded1d2dbf97a7@ec2-54-83-19-244.compute-1.amazonaws.com:5432/dai8c98gjvnfvv');
+    pgp('postgres://maphtdfqxqlbbj:a58153c65894b0a307027d96bc58e7c5d18a87bb389114239982e4b57c96374e@ec2-54-225-200-15.compute-1.amazonaws.com:5432/df2mefvft8a9di');
 if (!db) {
     console.log("Database doesn't exist!");
  process.exit(1);

ab9a12260d80df90259046273d5c45936430312d
DeepjotKaur <deepjotk@uw.edu>
2018-05-10 15:32:01 -0700

    added an endpoint to get all connections and added to index.js

---
+++ routes/getConnections.js
@@ -0,0 +1,49 @@
+//express is the framework we're going to use to handle requests
+const express = require('express');
+
+const delay = require('delay');
+//Create a new instance of express
+const app = express();
+
+//Create connection to Heroku Database
+let db = require('../utilities/utils').db;
+var router = express.Router();
+
+const bodyParser = require("body-parser");
+//This allows parsing of the body of POST requests, that are encoded in JSON
+app.use(bodyParser.json());
+
+router.post("/", (req, res) => {
+    let username = req.body['username']; //memberid_a is the reciever of request
+    let query = `SELECT username, firstname, lastname 
+                    FROM members 
+                    WHERE memberid 
+                    IN (
+                        SELECT memberid_b 
+                        FROM contacts 
+                        WHERE memberid_a=(
+                            SELECT memberid 
+                            FROM members 
+                            WHERE username=$1
+                        ) 
+                        AND verified=1
+                    )`;
+    
+    db.manyOrNone(query, [username])
+    .then(rows => {
+        res.send({
+            success: true,
+            recieved_requests: rows
+        })
+    })
+    .catch((err) => {
+        res.send({
+            message: "Something went wrong",
+            error: err
+        })
+    });
+    
+});
+
+
+module.exports = router;

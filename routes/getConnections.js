09e4b01c2cfafde6252ff4c119c99d676c199d37
Daryan <dhanshew@uw.edu>
2018-05-14 16:20:41 -0700

    Fixed get connections fuck you postgresql

--- routes/getConnections.js
+++ routes/getConnections.js
@@ -15,33 +15,49 @@ app.use(bodyParser.json());
 
 router.post("/", (req, res) => {
     let username = req.body['username'];
-    let query = `SELECT username, firstname, lastname, email 
-                    FROM members 
-                    WHERE memberid 
-                    IN (
-                        SELECT memberid_b 
-                        FROM contacts 
-                        WHERE memberid_a=(
-                            SELECT memberid 
-                            FROM members 
-                            WHERE username=$1)
-                        OR memberid
+    let query_a = `SELECT username, firstname, lastname 
+    FROM members 
+    WHERE memberid 
+    IN (
+        SELECT memberid_a 
+        FROM contacts 
+        WHERE memberid_b=(
+            SELECT memberid 
+            FROM members 
+            WHERE username=$1
+        ) 
+        AND verified=1
+    )`;
+    let query_b = `SELECT username, firstname, lastname 
+                        FROM members 
+                        WHERE memberid 
                         IN (
-                            SELECT memberid_a 
+                            SELECT memberid_b 
                             FROM contacts 
-                            WHERE memberid_b=(
+                            WHERE memberid_a=(
                                 SELECT memberid 
                                 FROM members 
-                                WHERE username=$1)
-                        ) 
-                        AND verified=1
-                    )`;
-    db.manyOrNone(query, [username])
-    .then(rows => {
-        res.send({
-            success: true,
-            recieved_requests: rows
+                                WHERE username=$1
+                            ) 
+                            AND verified=1
+                        )`;
+
+    var connections = [];
+    db.manyOrNone(query_a, [username])
+    .then(rows_a => {
+        db.manyOrNone(query_b, [username])
+        .then(rows_b => {
+            res.send( {
+                connections_a: rows_a,
+                connections_b: rows_b
+            })
         })
+        .catch((err) => {
+            res.send({
+                message: "Something went wrong",
+                error: err
+            })
+        });
     })
     .catch((err) => {
         res.send({

1f45e0c5ac0419ea2f2d9e9dc61733abf0b533a1
Daryan <dhanshew@uw.edu>
2018-05-15 11:08:57 -0700

    Tweak to getMessages

--- routes/getMessages.js
+++ routes/getMessages.js
@@ -8,15 +8,7 @@ var router = express.Router();
 
 router.post("/", (req, res) => {
     let chatname = req.body['chatname'];
-    /*let query = `SELECT Members.Username, Messages.Message,
-    to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD
-   HH24:MI:SS.US' ) AS Timestamp
-    FROM Messages
-   INNER JOIN Members ON Messages.MemberId=Members.MemberId
-   WHERE ChatId=$2 AND
-   Timestamp AT TIME ZONE 'PDT' > $1
-   ORDER BY Timestamp ASC`;*/
-   let query = `SELECT members.username, messages.message, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD
+    let query = `SELECT members.username, messages.message, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD
                 HH24:MI:SS.US' ) AS Timestamp FROM messages 
                 INNER JOIN members ON messages.memberid=members.memberid
                 WHERE messages.chatid=(SELECT chats.chatid FROM chats WHERE chats.name=$1)

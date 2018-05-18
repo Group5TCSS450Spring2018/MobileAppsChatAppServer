6816276a7b994064e887f9e8abeda93c40f3aae8
Tuan Dinh <tuandinhsemail@gmail.com>
2018-05-18 10:28:47 -0700

    fixed time stamp

--- routes/recentSent.js
+++ routes/recentSent.js
@@ -8,8 +8,8 @@ var router = express.Router();
 
 router.post("/", (req, res) => {
     let username = req.body['username'];
-    let query = `SELECT messages.message, messages.chatid, members.username, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD
-                 HH24:MI:SS.US' ) AS Timestamp, chats.name FROM messages
+    let query = `SELECT messages.message, messages.chatid, members.username, to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD HH24:MI:SS.US' ) 
+                 AS Timestamp, chats.name FROM messages
                  INNER JOIN chats ON chats.chatid=messages.chatid
                  INNER JOIN members ON messages.memberid=members.memberid
                  WHERE messages.chatid IN (SELECT chatid FROM chats WHERE chats.chatid IN 
